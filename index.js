e26e9d7b3b82da5fb3c3a88dd0e57fa54f694896
malahhh <vradosikmain@gmail.com>
2026-05-14 16:41:49 +0300

    загрузка вб страницы (безфп)

---
+++ index.js
@@ -0,0 +1,28 @@
+import { resolve } from "node:dns";
+import puppeteer from "puppeteer";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const browser = await puppeteer.launch(
+  {
+     headless: false,
+    }
+);
+const page = await browser.newPage();
+
+console.log("Иду на ВБ")
+
+await page.goto("https://www.wildberries.ru/", {
+  waitUntil: "networkidle0"
+});
+
+await page.waitForSelector("")
+
+await sleep(2000)
+  .then(() => {console.log("Проспал 2 сек")})
+
+
+await browser.close()
+console.log("Закрыл браузер")
